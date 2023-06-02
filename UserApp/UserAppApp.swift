0406408c265bc4b3c7424e289c05e7017b3505ca
William-Yulio <william.yulio331@gmail.com>
2023-06-02 17:52:05 +0700

    Initial Commit

---
+++ UserApp/UserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAppApp.swift
+//  UserApp
+//
+//  Created by William Yulio on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
